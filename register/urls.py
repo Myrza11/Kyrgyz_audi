6eae0b9994edd192eb71d2a88b3658f6d796dc55
davletmegahorosh <davletazimov142@gmail.com>
2024-02-15 10:45:15 +0600

    knu push

--- register/urls.py
+++ register/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from register.views import *
-from rest_framework_simplejwt.views import TokenBlacklistView
+from rest_framework_simplejwt.views import TokenBlacklistView, TokenRefreshView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view()),
     path('login/', CustomUserLoginView.as_view()),
-    path('refresh-token/', CustomUserTokenRefreshView.as_view()),
+    path('refresh-token/', TokenRefreshView.as_view()),
     path('forgot-password/', ForgotPasswordView.as_view()),
     path('reset-password/', ResetPasswordView.as_view()),
     path('change-password/', ChangePasswordView.as_view()),
@@ -13,4 +13,5 @@ urlpatterns = [
     path('logout/', TokenBlacklistView.as_view(), name='token_logout'),
     path('user-list/', UserListView.as_view()),
     path('user-update/<int:pk>/', UserUpdateView.as_view()),
+    path('user-profile/', UserProfileView.as_view(), name='user-profile'),
 ]
